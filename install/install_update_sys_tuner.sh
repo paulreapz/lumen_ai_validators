7a512ca15916b7ef499578456e033a9b82b16f6c
paulreapz <paulreapz@gmail.com>
2025-01-09 21:40:53 +0300

    Improved clarity

--- install/install_update_sys_tuner.sh
+++ install/install_update_sys_tuner.sh
@@ -1,79 +1,61 @@
 #!/bin/bash
 #set -x -e
 
-echo "###################### WARNING!!! ######################"
-echo "###   This script will install the Solana Sys Tuner  ###"
-echo "###   for the Solana Validator.                       ###"
-echo "########################################################"
+# Warning and Information
+cat << "EOF"
+###################### WARNING!!! ######################
+###   This script will install the Solana Sys Tuner  ###
+###   for the Solana Validator.                      ###
+########################################################
+EOF
 
-install_validator () {
+install_sys_tuner() {
 
+  # Clean previous manager setup
   rm -rf sv_manager/
 
-  if [[ $(which apt | wc -l) -gt 0 ]]
-  then
-  pkg_manager=apt
-  elif [[ $(which yum | wc -l) -gt 0 ]]
-  then
-  pkg_manager=yum
+  # Detect package manager
+  if [[ $(which apt | wc -l) -gt 0 ]]; then
+    pkg_manager=apt
+  elif [[ $(which yum | wc -l) -gt 0 ]]; then
+    pkg_manager=yum
   fi
 
-  echo "Updating packages..."
+  echo "### Updating packages... ###"
   $pkg_manager update
-  echo "Installing ansible, curl, unzip..."
+  echo "### Installing dependencies... ###"
   $pkg_manager install ansible curl unzip --yes
 
-  ansible-galaxy collection install ansible.posix
-  ansible-galaxy collection install community.general
+  ansible-galaxy collection install ansible.posix community.general
 
-  echo "Downloading Solana validator manager version $sv_manager_version"
+  echo "### Downloading Solana validator manager ###"
   cmd="https://github.com/mfactory-lab/sv-manager/archive/refs/tags/$sv_manager_version.zip"
-  echo "starting $cmd"
   curl -fsSL "$cmd" --output sv_manager.zip
-  echo "Unpacking"
+  echo "### Unpacking manager ###"
   unzip ./sv_manager.zip -d .
-
   mv sv-manager* sv_manager
   rm ./sv_manager.zip
-  cd ./sv_manager || exit
+  cd sv_manager || exit 1
   cp -r ./inventory_example ./inventory
 
-  # shellcheck disable=SC2154
-  #echo "pwd: $(pwd)"
-  #ls -lah ./
+  ansible-playbook --connection=local --inventory ./inventory/mainnet.yaml --limit localhost playbooks/pb_install_validator.yaml --tags validator.service.sys-tuner
 
-
-  ansible-playbook --connection=local --inventory ./inventory/mainnet.yaml --limit localhost  playbooks/pb_install_validator.yaml --tags validator.service.sys-tuner
-
-  echo "### 'Uninstall ansible ###"
-
+  echo "### Cleaning up Ansible installation ###"
   $pkg_manager remove ansible --yes
-  
-  echo "### Solana Sys Tuner Servie installed. You can check the status with 'systemctl status solana-sys-tuner' ###"
 
-
+  echo "### Solana Sys Tuner Service installed. Check status with: 'systemctl status solana-sys-tuner' ###"
 }
 
-
-while [ $# -gt 0 ]; do
-
-   if [[ $1 == *"--"* ]]; then
-        param="${1/--/}"
-        declare ${param}="$2"
-  #      echo $1 $2 // Optional to see the parameter:value result
-   fi
-
-  shift
-done
-
+# Default manager version
 sv_manager_version=${sv_manager_version:-latest}
 
-echo "installing sv manager version $sv_manager_version"
-
-echo "This script will setup the Solana Sys Tuner Service with default parameters. Proceed?"
+# Main script logic
+cat << "EOF"
+This script will set up the Solana Sys Tuner Service with default parameters. Proceed?
+EOF
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) install_validator "$sv_manager_version" "$extra_vars" "$solana_version" "$tags" "$skip_tags"; break;;
-        No ) echo "Aborting install. No changes will be made."; exit;;
+        Yes ) install_sys_tuner; break;;
+        No ) echo "Aborting installation. No changes made."; exit 0;;
     esac
 done
